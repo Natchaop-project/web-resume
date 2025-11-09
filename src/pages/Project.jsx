7f3cb57bf1132424e917c64c6dbda0de0e563a50
Natchanop <domenatchanop@gmail.com>
2025-11-09 15:57:35 +0700

    edit color deme button

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -7,7 +7,7 @@ function Project() {
       <section className="mt-4 rounded-2xl p-10 shadow-lg">
               <h3 className="mb-4 text-xl font-bold text-shadow-2xs">Projects</h3>
 
-              <div className="">
+              <div>
                 <article>
                   <h4 className="text-sm font-semibold">Final Project (2023)</h4>
                   <p className="text-sm">Recommender System</p>
@@ -15,9 +15,9 @@ function Project() {
                     Developed a movie recommendation system using algorithms and user data to analyze preferences and suggest movies that best match users’ interests.
                   </p>
                   <div>
-                    <a className='rounded-full border-2 px-2 hover:bg-gray-700 hover:text-white' href="https://web-movies-recommender-system.vercel.app/">demo</a>
+                    <a className='bg-sky-500 rounded-full shadow-xl hover:shadow-none text-white px-2 py-1 hover:bg-gray-700' href="https://web-movies-recommender-system.vercel.app/">demo</a>
                   </div>
-                  <div className='md:flex gap-2'>
+                  <div className='my-2 md:flex gap-2 '>
                     <img className='w-2/5 size-cover' src={Img1} alt="" />
                     <img className='w-2/5 size-cover' src={Img2} alt="" />
                   </div>
